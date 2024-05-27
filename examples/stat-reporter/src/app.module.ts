003a2450e277651f56cdadd6b121e62c9b8e5b77
Roman Khabarov <roman@globalart.dev>
2024-05-27 22:43:44 +0200

    feat: stat reporter example

---
+++ examples/stat-reporter/src/app.module.ts
@@ -0,0 +1,35 @@
+import { GatewayIntentBits, Partials } from 'discord.js';
+import { NestCordModule } from '../../../packages/core';
+import { Module } from '@nestjs/common';
+import { AppGateway } from './app.gateway';
+import { NestCordStatReporterModule } from '../../../packages/stat-reporter';
+import { CronExpression } from '@nestjs/schedule';
+
+@Module({
+  imports: [
+    NestCordModule.forRoot({
+      token: process.env.DISCORD_TOKEN,
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
+      ],
+      partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+    }),
+    NestCordStatReporterModule.forRoot({
+      services: [
+        {
+          name: 'top.gg',
+          url: 'https://top.gg/bots/:bot_id/stats',
+          bodyData: { server_count: '{{serverCount}}', shards: '{{shardCount}}' },
+          schedule: CronExpression.EVERY_5_MINUTES,
+        },
+      ],
+    }),
+  ],
+  providers: [AppGateway],
+})
+export class AppModule {}
