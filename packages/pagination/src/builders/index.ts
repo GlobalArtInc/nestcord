7b94ea2bd6b40ee8a59a5390b0af6159dd815fb3
Roman Khabarov <roman@ga-games.com>
2024-06-23 12:23:44 +0200

    fix(pagination) add index file

--- packages/pagination/src/builders/index.ts
+++ packages/pagination/src/builders/index.ts
@@ -1,3 +1,3 @@
-import { PaginatorTypeEnum } from '../enums';
-import { ButtonsPaginationBuilder } from './buttons-padgination.builder';
-import { SelectMenuPaginationBuilder } from './select-menu-pagination.builder';
+export * from './buttons-padgination.builder';
+export * from './select-menu-pagination.builder';
+export * from './page.builder';
--- packages/pagination/src/index.ts
+++ packages/pagination/src/index.ts
@@ -1,6 +1,7 @@
 export * from './enums';
 export * from './exceptions';
 export * from './interfaces';
+export * from './builders';
 
 export * from './nestcord-pagination.module';
 export * from './nestcord-pagination.service';
