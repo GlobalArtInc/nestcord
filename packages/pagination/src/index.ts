a49624eeb86f97b7a5d6cd42df35e639387f8e27
Roman Khabarov <roman@globalart.dev>
2024-06-24 11:34:14 +0000

    fix(pagination): add forRoot and forFeature implement

--- packages/pagination/src/index.ts
+++ packages/pagination/src/index.ts
@@ -1,3 +1,5 @@
+import { NESTCORD_PAGINATION_OTPIONS } from './nestcord-pagination.module-definition';
+
 export * from './enums';
 export * from './exceptions';
 export * from './interfaces';
@@ -5,3 +7,5 @@ export * from './builders';
 
 export * from './nestcord-pagination.module';
 export * from './nestcord-pagination.service';
+
+export { NESTCORD_PAGINATION_OTPIONS };
